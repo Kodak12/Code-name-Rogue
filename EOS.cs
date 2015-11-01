269f37f4cd2a59c75c242dd91ea75c6e9d29bbb5
Chuk Ume <Chubiyke@gmail.com>
2015-10-31 22:48:54 -0400

    update

--- EOS.cs
+++ EOS.cs
@@ -29,7 +29,7 @@ namespace homework5
         }
 
         public double virialTrunc(double P, double T)
-        {   // implementation for SO2 because it the compound of interest at moment
+        {   // implementation for SO2 because it is the compound of interest at moment
             // calculates the molar volume using the
             // virail truncated EOS 
             // units are in SI
